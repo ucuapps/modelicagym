dbfe8cb9dcad3bc0db32bb68511557da203f1774
OlehLuk <oleh.lukianykhin.a@gmail.com>
2019-10-17 13:22:22 +0300

    Added proper packaging for pip install

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='modelica-gym',
+    version='1.0',
+    packages=['examples', 'modelicagym.environment'],
+    url='',
+    license='',
+    author='Oleh',
+    author_email='',
+    description='Library for Reinforcement Learning on Modelica models using OpenAI Gym'
+)
