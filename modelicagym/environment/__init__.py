813460da6203c14efd2229d42ab84e87290355df
OlehLuk <oleh.lukianykhin.a@gmail.com>
2020-10-01 21:09:08 +0300

    Added Model Exchange FMU wrappers

--- modelicagym/environment/__init__.py
+++ modelicagym/environment/__init__.py
@@ -1,2 +1,3 @@
-from .modelica_base_env import *
-from .modelica_cs_env import *
+from .modelica_base_env import ModelicaBaseEnv, ModelicaType, FMIStandardVersion
+from .modelica_cs_env import FMI1CSEnv, FMI2CSEnv, ModelicaCSEnv
+from .modelica_me_env import ModelicaMEEnv, FMI1MEEnv, FMI2MEEnv
