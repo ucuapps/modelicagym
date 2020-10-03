848233a1d89cb11d0daadabc1f5962487303e630
OlehLuk <oleh.lukianykhin.a@gmail.com>
2020-10-03 15:51:59 +0300

    DQN cartpole experiments with buffer size

--- examples/cart_pole_dqn_experiments.py
+++ examples/cart_pole_dqn_experiments.py
@@ -129,7 +129,7 @@ if __name__ == "__main__":
                                      log_level=logging.INFO,
                                      binning=False,
                                      mode="CS",
-                                     exp_id="n5")
+                                     exp_id="n6")
 
     end = time.time()
     print("Total execution time {:.2f} seconds".format(end-start))
